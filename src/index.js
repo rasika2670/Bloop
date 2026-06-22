b98ed3cf104b88a8f74e77039ae70e07eab2c406
Rasika Mhaske <162533669+rasika2670@users.noreply.github.com>
2026-06-22 12:38:51 +0530

    feat: implement user registration route with async error handling

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+});
 
 
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,12 @@
-const asyncHandler = (requestHandler) => (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
-        res.status(error.code || 500).json({
-            success: false, 
-            message: "Internal Server Error" 
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+            res.status(error.code || 500).json({
+                success: false, 
+                message: "Internal Server Error" 
+            });
         });
-    });
+    };
 };
 
 export { asyncHandler };
